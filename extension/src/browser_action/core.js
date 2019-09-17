01696a416b94617611309387461c24e14d2ee980
addyosmani <addyosmani@gmail.com>
2019-09-17 16:07:20 -0700

    (extension) Implement MVP with results

---
+++ extension/src/browser_action/core.js
@@ -0,0 +1,76 @@
+const API_KEY = 'AIzaSyCorXZgoGduhFmi18X2W77zNultjh7O2xI';
+const API_URL = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?';
+let encodedUrl = '';
+
+function fetchReportForTab() {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, tabs => {
+        const url = tabs[0].url;
+        console.log(`Active tab URL is ${url}`);
+        encodedUrl = encodeURIComponent(url);
+        fetchAPIResults(encodedUrl);
+        // TODO: Show a loading state?
+    });
+}
+
+async function fetchAPIResults(url) {
+    const query = [
+        'url=url%3A' + url,
+        'key=' + API_KEY,
+    ].join('&');
+    const queryURL = API_URL + query;
+
+    console.log(`Fetching PSI results from ${queryURL}`);
+    const response = await fetch(queryURL);
+    const json = await response.json();
+    console.log(`Response from PSI was ${json}`);
+    processResults(json);
+}
+
+function processResults(result) {
+    const experience = result.loadingExperience;
+    const metrics = experience.metrics;
+    const overall_category = experience.overall_category;
+    const fcp = metrics.FIRST_CONTENTFUL_PAINT_MS;
+    const fid = metrics.FIRST_INPUT_DELAY_MS;
+
+    const fcp_template = buildDistributionTemplate(fcp, 'First Contentful Paint (FCP)');
+    const fid_template = buildDistributionTemplate(fid, 'First Input Delay (FID)');
+    const link_template = buildPSILink();
+    const tmpl = `${fcp_template} ${fid_template} ${link_template}`;
+    const el = document.getElementById('report');
+    el.innerHTML = tmpl;
+
+}
+
+function buildDistributionTemplate(metric, label) {
+    return `<div class="field-data">
+    <div class="metric-wrapper lh-column">
+      <div class="lh-metric">
+        <div class="field-metric ${metric.category.toLowerCase()} lh-metric__innerwrap">
+          <span class="metric-description">${label}: ${metric.category.toLowerCase()}</span>
+          <div hidden class="metric-value lh-metric__value">${metric.category.toLowerCase()}</div></div>
+        <div class="metric-chart">
+          <div class="bar fast" style="flex-grow: 
+          ${Math.floor(metric.distributions[0].proportion * 100)};">
+          ${Math.floor(metric.distributions[0].proportion * 100)}%</div>
+          <div class="bar average" style="flex-grow: 
+          ${Math.floor(metric.distributions[1].proportion * 100)};">
+          ${Math.floor(metric.distributions[1].proportion * 100)}%</div>
+          <div class="bar slow" style="margin-left:-10px;flex-grow: 
+          ${Math.floor(metric.distributions[2].proportion * 100)};">
+          ${Math.floor(metric.distributions[2].proportion * 100)}%</div>
+        </div></div>
+      </div>
+    </div> `;
+}
+
+function buildPSILink() {
+    return `<br><a href='https://developers.google.com/speed/pagespeed/insights/?url=${encodedUrl}' target='_blank'>click to view detailed analysis</a>`;
+}
+
+window.addEventListener('load', () => {
+    fetchReportForTab();
+});
